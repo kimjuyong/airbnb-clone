9e017f0ebf8ad61f7623840662d35895a309cba8
juyongkim <kimwtgr@gmail.com>
2019-11-12 15:56:42 +0900

    complete admin

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core import models as core_models
 
+
 class List(core_models.TimeStampedModel):
 
     """ List Model Definition """
@@ -11,3 +12,8 @@ class List(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def count_rooms(self):
+        return self.rooms.count()
+
+    count_rooms.short_description = "Number Of Rooms"
--- reservations/models.py
+++ reservations/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 from core import models as core_models
+
 
 class Reservation(core_models.TimeStampedModel):
 
     """ Reservation Model Definition """
-
 
     STATUS_PENDING = "pending"
     STATUS_CONFIRMED = "confirmed"
@@ -15,13 +16,27 @@ class Reservation(core_models.TimeStampedModel):
         (STATUS_CONFIRMED, "Confirmed"),
         (STATUS_CANCELED, "Canceled"),
     )
-    
-    status = models.CharField(max_length=12, choices=STATUS_CHOICE, default=STATUS_PENDING)
+
+    status = models.CharField(
+        max_length=12, choices=STATUS_CHOICE, default=STATUS_PENDING
+    )
 
     check_in = models.DateField()
     check_out = models.DateField()
-    guest = models.ForeignKey("users.User",on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room",on_delete=models.CASCADE)
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.room} - {self.check_in}"
+
+    def in_progress(self):
+        now = timezone.now().date()
+        return now > self.check_in and now < self.check_out
+
+    in_progress.boolean = True
+
+    def is_finished(self):
+        now = timezone.now().date()
+        return now > self.check_out
+
+    is_finished.boolean = True
