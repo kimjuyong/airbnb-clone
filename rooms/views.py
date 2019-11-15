cb160862d1a057c3f0818c25f33bc7335d029c0d
juyongkim <kimwtgr@gmail.com>
2019-11-15 17:24:59 +0900

    first search view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,28 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
+from . import models
 
-# Create your views here.
+
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
+
+class RoomDetail(DetailView):
+
+    """ RoomDetail Definition """
+
+    model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", context={"city":city})
+
